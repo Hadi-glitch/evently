ae775e19610718c7b6b3066c58ca9a831d085694
Hadi <abdulhadi74737@gmail.com>
2024-08-29 13:12:10 +0500

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex flex-center wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+        <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="logo"
+          />
+        </Link>
+
+        <p>2024 Evently. All rigths reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavItems = () => {
+    const pathname = usePathname()
+
+  return (
+    <ul className="flex md:flex-between w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        const isActive = pathname === link.route
+
+        return (
+          <li key={link.route}>
+            <Link className={`${isActive && "text-primary-500"} flex-center p-medium-16 whitespace-nowrap`} href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default NavItems;
