c7c1fe90a199150101ee0a674772f5e5214ca920
Hadi <abdulhadi74737@gmail.com>
2024-09-06 00:39:19 +0500

    finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,16 +3,21 @@ import Image from "next/image";
 import Link from "next/link";
 import Collection from "@/components/shared/Collection";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import Search from "@/components/shared/Search";
+import { SearchParamProps } from "@/types";
+import CategoryFilter from "@/components/shared/CategoryFilter";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
   const events = await getAllEvents({
-    query : "",
-    category : "",
-    page : 1,
-    limit : 6
-  })
-
-  console.log(events)
+    query: '',
+    category,
+    page,
+    limit: 6,
+  });
 
   return (
     <>
@@ -49,19 +54,19 @@ export default async function Home() {
           Trust by <br /> Thousands of Events
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search
-          CategoryFilter
+          <Search />
+          <CategoryFilter />
         </div>
 
-        <Collection 
+        <Collection
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
           limit={6}
           page={1}
-          totalPages={2}
-         />
+          totalPages={events?.totalPages}
+        />
       </section>
     </>
   );
--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,24 +1,22 @@
-import React, { useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { checkoutOrder } from "@/lib/actions/order.actions";
+import React, { useEffect } from 'react'
+import { loadStripe } from '@stripe/stripe-js';
 
-import { Button } from "../ui/button";
-import { IEvent } from "@/lib/database/models/event.model";
+import { IEvent } from '@/lib/database/models/event.model';
+import { Button } from '../ui/button';
+import { checkoutOrder } from '@/lib/actions/order.actions';
 
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
-const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
   useEffect(() => {
     // Check to see if this is a redirect back from Checkout
     const query = new URLSearchParams(window.location.search);
-    if (query.get("success")) {
-      console.log("Order placed! You will receive an email confirmation.");
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.');
     }
 
-    if (query.get("canceled")) {
-      console.log(
-        "Order canceled -- continue to shop around and checkout when you’re ready."
-      );
+    if (query.get('canceled')) {
+      console.log('Order canceled -- continue to shop around and checkout when you’re ready.');
     }
   }, []);
 
@@ -28,19 +26,20 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
       eventId: event._id,
       price: event.price,
       isFree: event.isFree,
-      buyerId: userId,
-    };
+      buyerId: userId
+    }
 
+    // console.log(order)
     await checkoutOrder(order);
-  };
+  }
 
   return (
-    <form action={onCheckout}>
+    <form action={onCheckout} method="post">
       <Button type="submit" role="link" size="lg" className="button sm:w-fit">
-        {event.isFree ? "Get Ticket" : "Buy Ticket"}
+        {event.isFree ? 'Get Ticket' : 'Buy Ticket'}
       </Button>
     </form>
-  );
-};
+  )
+}
 
-export default Checkout;
+export default Checkout
